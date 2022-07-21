ca1bed306c704416a7ad70fc518402203a61ca9f
vignesh pugazhendhi <irarelycode@gmail.com>
2022-07-21 20:49:56 +0530

    drawer navigation completed

--- Screens/DrawerNavigatorScreens/Feed.jsx
+++ Screens/DrawerNavigatorScreens/Feed.jsx
@@ -1,10 +1,12 @@
 import { View, Text } from "react-native";
+import {useDrawerStatus} from "@react-navigation/drawer";
 import React from "react";
 
 const Feed = () => {
   return (
     <View>
       <Text>Feed</Text>
+      {/* <Text>{alert(useDrawerStatus()==="open")}</Text> */}
     </View>
   );
 };
